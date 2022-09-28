e33138514e78c2134672f1473ff83fefe1313f0a
Sabit Can Turunc <114212366+Sharkener@users.noreply.github.com>
2022-09-29 00:00:58 +0300

    Add files via upload

---
+++ ClassesExercise/src/MainClass.java
@@ -0,0 +1,13 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		operations islemler = new operations();
+
+		System.out.println(islemler.topla(5, 7));
+		System.out.println(islemler.cikar(5, 7));
+		System.out.println(islemler.carp(5, 7));
+		System.out.println(islemler.bol(14, 7));
+
+	}
+}

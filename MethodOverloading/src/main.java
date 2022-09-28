02d98db603c47536f884497ce659870045bf947c
Sabit Can Turunc <114212366+Sharkener@users.noreply.github.com>
2022-09-29 00:02:54 +0300

    Add files via upload

---
+++ MethodOverloading/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		fourOperations dortislem= new fourOperations();
+		
+		System.out.println(dortislem.topla(5,7));
+		System.out.println(dortislem.topla(5,7,5));
+		
+	}
+
+}
